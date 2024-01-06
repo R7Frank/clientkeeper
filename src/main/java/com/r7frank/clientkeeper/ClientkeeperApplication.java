113d5b9eb65180c9bc060d714d68a23e6d0e494b
R7Frank <r7frank@hotmail.com>
2024-01-05 23:11:06 -0300

    Criados: projeto e entidade Client

---
+++ src/main/java/com/r7frank/clientkeeper/ClientkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.r7frank.clientkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientkeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r7frank/clientkeeper/ClientkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r7frank.clientkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
